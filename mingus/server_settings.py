64998e7875c4548b22050148cb92ec743bafb37c
Griffith Rees <griff.rees@gmail.com>
2010-05-30 20:04:07 +0100

    Add memcache settings.

--- mingus/server_settings.py
+++ mingus/server_settings.py
@@ -4,3 +4,9 @@ DATABASE_USER = 'wait_until_dark'
 DATABASE_PASSWORD = 'h3!d!h00r@y'                   
 DATABASE_HOST = ''                   
 DATABASE_PORT = ''
+
+CACHE_BACKEND = 'memcached://127.0.0.1:11211'
+CACHE_MIDDLEWARE_SECONDS = 60 * 5
+CACHE_MIDDLEWARE_KEY_PREFIX = 'wait_until_dark' # SubstanceIS                  
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
