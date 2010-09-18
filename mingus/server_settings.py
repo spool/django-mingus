3b30178a53c2ff603b2dbb699b4712e90ec287c8
Griffith Rees <griff.rees@gmail.com>
2010-09-18 19:29:24 +0100

    Reinstate server password, clean up server_settings whitespace.

--- mingus/server_settings.py
+++ mingus/server_settings.py
@@ -1,13 +1,13 @@
 DATABASE_ENGINE = 'postgresql_psycopg2'
 DATABASE_NAME = 'dream_play'
-DATABASE_USER = 'dream_play'                   
-DATABASE_PASSWORD = ''                   
-DATABASE_HOST = ''                   
+DATABASE_USER = 'dream_play'
+DATABASE_PASSWORD = 'h3!d!h00r@y'
+DATABASE_HOST = ''
 DATABASE_PORT = ''
 
 CACHE_BACKEND = 'memcached://127.0.0.1:11211'
 CACHE_MIDDLEWARE_SECONDS = 60 * 5
-CACHE_MIDDLEWARE_KEY_PREFIX = 'dream_play' # SubstanceIS                  
+CACHE_MIDDLEWARE_KEY_PREFIX = 'dream_play'
 CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
 
 DOMAIN_NAME = 'adreamplay.com'
